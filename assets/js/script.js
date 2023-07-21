5cf30c410ab7ee4941cb164702f637a979a608e8
Jacob Medina <jacobmedina1618@gmail.com>
2023-07-21 15:24:59 -0400

    added upgraded armor, mini bosses, and monsters data

--- assets/js/script.js
+++ assets/js/script.js
@@ -107,11 +107,17 @@ class ChallengeType {
         if (uniqueName) this.name = uniqueName;
         else this.name = this.category[0];
 
-        if (filterFunc) this.filterFunc = filterFunc; 
+        if (filterFunc) this.filterFunc = filterFunc;
         if (calcAmount) this.calcAmount = calcAmount;
+        else this.calcAmount = () => 1;
+
+        this.entries = null;
     }
 
     getEntries(data) {
+        if (this.entries) return this.entries;
+        if (!data) return null;
+
         // filter by category
         let filteredData = [];
         for (const cat of this.category) {
@@ -120,14 +126,29 @@ class ChallengeType {
         filteredData = filteredData.flat();
 
         // filter data by filterFunc (if applicable)
-        if (this.filterFunc) return filteredData.filter(this.filterFunc);
-        else return filteredData;
+        this.entries = filteredData;
+        if (this.filterFunc) this.entries = filteredData.filter(this.filterFunc);
+
+        return this.entries;
+    }
+
+    getRandomEntry(seededRandom) {
+        return this.getEntries()[seededRandom.randInt(this.getEntries().length)];
     }
 }
 
 let rand;
+
+const uniqueEquipment = [
+    'sword of the hero', "biggoron's sword", "sea-breeze shield",
+    "fierce deity sword", 'scimitar of the seven', 'white sword of the sky',
+    'lightscale trident', 'sea-breeze boomerang', 'daybreaker', 'hylian shield',
+    'great eagle bow', 'boulder breaker', 'dusk bow', 'master sword'
+];
+
 const challengeTypes = [
-    // new ChallengeType('main quests', 24, 24),
+    // challenges with 1 item
+    new ChallengeType('main quests', 24, 24),
     new ChallengeType('side quests', 5, 8),
     new ChallengeType('shrine quests', 8, 12),
     new ChallengeType('side adventures', 12, 20),
@@ -135,20 +156,79 @@ const challengeTypes = [
     new ChallengeType('autobuild', 19, 19),
     new ChallengeType('creatures', 13, 13, 'patricia', entry => entry.name === 'patricia'),
     new ChallengeType('monsters', 23, 23, 'bosses', entry => {
-        const id = entry.id;
-        const isBossOrGanondorf = (id >= 191 && id <= 201)
+        const {id} = entry;
+        const isBossOrGanondorf = isBetween(id, 191, 201);
         const isKohga = (id === 165);
         return isBossOrGanondorf || isKohga;
     }),
     new ChallengeType(['monsters', 'creatures'], 9, 9, 'special horse/dragon/dondon', entry => {
-        const id = entry.id;
-        const name = entry.name;
-        const isSpecialHorse = (id >= 2 && id <= 5);
-        const isDragon = ((id >= 188 && id <= 190) || id === 202)
+        const {id, name} = entry;
+        const isSpecialHorse = isBetween(id, 2, 5);
+        const isDragon = (isBetween(id, 188, 190) || id === 202);
         const isDondon = (name === 'dondon');
         return isSpecialHorse || isDragon || isDondon;
     }),
-
+    new ChallengeType('monsters', 3, 3, 'training construct', entry => entry.id === 159),
+    new ChallengeType('armor', 12, 14),
+    new ChallengeType('armor', 16, 16, 'armor ★', entry => entry.upgradable),
+    new ChallengeType('equipment', 14, 16, 'unique equipment', entry => uniqueEquipment.includes(entry.name)),
+    
+    // challenges with 1 or more items
+    new ChallengeType('korok seeds', 0, 24, undefined, undefined,
+        difficulty => clamp(2 * difficulty, 1, 1000)),
+    new ChallengeType('shrines', 0, 24, undefined, undefined,
+        difficulty => clamp(Math.ceil(1.3 * difficulty), 1, 152)),
+    new ChallengeType('towers', 0, 24, undefined, undefined,
+        difficulty => clamp(Math.ceil(difficulty / 2.5), 1, 15)),
+    new ChallengeType('lightroots', 0, 24, undefined, undefined,
+        difficulty => clamp(Math.ceil(difficulty / 1.3), 1, 120)),
+    new ChallengeType('cherry blossom trees', 0, 20, undefined, undefined,
+        difficulty => clamp(Math.ceil(difficulty / 1.3), 1, 8)),
+    new ChallengeType('caves', 0, 24, undefined, undefined,
+        difficulty => clamp(Math.ceil(1.3 * difficulty), 1, 147)),
+    new ChallengeType('yiga schematics', 0, 24, undefined, undefined,
+        difficulty => clamp(Math.ceil(difficulty / 2), 1, 34)),
+    new ChallengeType('hudson signs', 0, 24, undefined, undefined,
+        difficulty => clamp(Math.ceil(1.3 * difficulty), 1, 81)),
+    new ChallengeType("sage's wills", 0, 24, undefined, undefined,
+        difficulty => clamp(Math.ceil(difficulty / 3), 1, 20)),
+    new ChallengeType('creatures', 0, 10, undefined,
+        entry => {
+            const {id, name} = entry;
+            const isSpecialHorse = (id >= 2 && id <= 5);
+            const isDondon = (name === 'dondon');
+            const isPatricia = (name === 'patricia');
+            return !isSpecialHorse && !isDondon && !isPatricia;
+        }, 
+        difficulty => Math.max(Math.ceil(1.3 * difficulty), 1)),
+    new ChallengeType('monsters', 4, 16, 'mini bosses',
+        entry => {
+            const {id, name} = entry;
+            const words = name.split(" ");
+            const isMiniBoss = isBetween(id, 147, 150) || isBetween(id, 160, 162) || (isBetween(id, 169, 187) && id !== 184) || words.includes('lynel');
+            const isSilver = words.includes('silver') || words.includes('iv');
+            return isMiniBoss || isSilver;
+        },
+        difficulty => clamp(Math.ceil(difficulty / 5), 1, 3)),
+    new ChallengeType('monsters', 0, 16, undefined,
+        entry => {
+            const {id, name} = entry;
+            const words = name.split(" ");
+            const isBoss = isBetween(id, 191, 201) || (id === 165);
+            const isDragon = (isBetween(id, 188, 190) || id === 202);
+            const isMiniBoss = isBetween(id, 147, 150) || isBetween(id, 160, 162) || (isBetween(id, 169, 187) && id !== 184) || words.includes('lynel');
+            const isSilver = words.includes('silver') || words.includes('iv');
+            return !isBoss && !isDragon && !isMiniBoss && !isSilver;
+        },
+        difficulty => Math.max(difficulty, 1)),
+    new ChallengeType('equipment', 2, 16, undefined,
+        entry => !uniqueEquipment.includes(entry), 
+        difficulty => Math.max(Math.floor(difficulty / 1.3), 1)),
+    new ChallengeType('equipment', 10, 18, 'equipment ✧',
+        entry => entry.name.endsWith('(new)'),
+        difficulty => Math.max(Math.ceil(difficulty / 4), 1)),
+    new ChallengeType('materials', 0, 10, undefined, undefined,
+        difficulty => Math.max(Math.ceil(1.3 * difficulty), 1))
 ];
 
 
@@ -183,14 +263,30 @@ function replaceNewSeed() {
 async function fetchAndGenerateBoard(size=5) {
     const data = await fetchData();
     magicSquare = new MagicSquare(size);
-    const entries = getRandomEntries(size * size, data);
-    console.log(entries);
-    generateBingoBoard(size, entries);
-
+    const entries = []; //getRandomEntries(size * size, data);
+    
     for (const challengeType of challengeTypes) {
         console.log(challengeType.name, challengeType.getEntries(data));
     }
+    
+    for (const difficulty of magicSquare._matrix.flat()) {
+        const validChallengeTypes = challengeTypes.filter(challengeType => (difficulty >= challengeType.diffMin && difficulty <= challengeType.diffMax));
+        const randChallengeType = validChallengeTypes[rand.randInt(validChallengeTypes.length)];
+        let randEntry = randChallengeType.getRandomEntry(rand);
+        randEntry = structuredClone(randEntry);
+        randEntry.challengeType = randChallengeType.name;
+        randEntry.difficulty = difficulty;
+        randEntry.amount = randChallengeType.calcAmount(difficulty);
+        console.log(randEntry);
+        // TODO: get unique entries
+        entries.push(randEntry);
+        // console.log('Difficulty:', difficulty, );
+    }
 
+    console.log(entries);
+
+    generateBingoBoard(size, entries);
+    
     endLoading();
 }
 
@@ -210,6 +306,8 @@ function fetchData() {
         });
 
     if (!runningLocal) {
+        const mainQuestsPromise = fetch('./assets/data/main-quests.json')
+        .then(response => response.json());
 
         const sideQuestsPromise = fetch('./assets/data/side-quests.json')
         .then(response => response.json());
@@ -229,7 +327,7 @@ function fetchData() {
         const miscItemsPromise = fetch('./assets/data/misc-items.json')
         .then(response => response.json());
         
-        return Promise.all([compendiumPromise, sideQuestsPromise, sideAdventuresPromise, shrineQuestsPromise, armorPromise, armorSetsPromise, miscItemsPromise])
+        return Promise.all([compendiumPromise, mainQuestsPromise, sideQuestsPromise, sideAdventuresPromise, shrineQuestsPromise, armorPromise, armorSetsPromise, miscItemsPromise])
         .then(data => data.flat());
     }
 
@@ -261,10 +359,12 @@ function generateBingoBoard(size=5, entries) {
 
         const challengeText = $('<p class="challenge-text">');
         const itemText = $('<p class="item-text">');
+        const difficultyText = $('<p class="item-text">');
         
         const challenge = getChallenge(entry);
         challengeText.text(challenge.challenge);
         itemText.text(challenge.entry);
+        difficultyText.text(entry.difficulty);
 
         boardItem.append(challengeText, itemText);
         $('.bingo-board').append(boardItem);
@@ -273,15 +373,18 @@ function generateBingoBoard(size=5, entries) {
 
 
 // returns challenge text appropriate to the type of entry
-function getChallenge({category, id, name, edible}) {
+function getChallenge({category, id, name, edible, amount, challengeType}) {
+    // normal main quests
+    if (category === "main quests") return {challenge: `main quest:`, entry: `'${name}'`};
+
     // normal side adventues
-    if (category === "side adventures") return {challenge: `side adventure:`, entry: name};
+    if (category === "side adventures") return {challenge: `side adventure:`, entry: `'${name}'`};
 
     // normal side quests
-    if (category === "side quests") return {challenge: `side quest:`, entry: name};
+    if (category === "side quests") return {challenge: `side quest:`, entry: `'${name}'`};
 
     // normal shrine quests
-    if (category === "shrine quests") return {challenge: `shrine quest:`, entry: name};
+    if (category === "shrine quests") return {challenge: `shrine quest:`, entry: `'${name}'`};
 
     // normal armor sets
     if (category === "armor sets") return {challenge: `obtain the`, entry: `${name} Set`};
@@ -310,9 +413,10 @@ function getChallenge({category, id, name, edible}) {
     if (uniqueEquipment.includes(name)) return {challenge: "obtain the", entry: titleCase(name)};
     
     // armor
+    if (challengeType === 'armor ★') return {challenge: "obtain the", entry: `${titleCase(name)}★`};
     if (category === "armor") return {challenge: "obtain the", entry: titleCase(name)};
     
-    let amount = rand.randInt(5) + 1;
+    // let amount = //rand.randInt(5) + 1;
     name = getPlural(amount, simplifyName(name, id));
     name = titleCase(romanNumeral(name));
 
@@ -328,7 +432,7 @@ function getChallenge({category, id, name, edible}) {
     if ([14, 18, 19, 29].includes(id)) return {challenge: `feed ${amount}`, entry: titleCase(name)};  // white goats, hateno cows, hylian retriever
     if (id === 52) return {challenge: `anger ${amount}`, entry: titleCase(name)};  // coocoos
     if ([-8, -10, -17].includes(id)) return {challenge: `collect ${amount}`, entry: titleCase(name)};  // sage's will, yiga schematic, korok seeds
-    if ([-12, -14, -15].includes(id)) return {challenge: `activate ${amount}`, entry: titleCase(name)};  // towers, cherry blossoms, nightroot
+    if ([-12, -14, -15].includes(id)) return {challenge: `activate ${amount}`, entry: titleCase(name)};  // towers, cherry blossoms, lightroot
     if (id === -9) return {challenge: `install ${amount}`, entry: titleCase(name)};  // Hudson signs
     if (category === "shrines") return {challenge: `complete ${amount}`, entry: titleCase(name)};  // shrines
 
@@ -430,6 +534,15 @@ function hideElement(element) {
 
 function showElement(element) {
     $(element).removeClass("hide");
+}
+
+
+function clamp(value, min, max) {
+    return Math.min(Math.max(value, min), max);
+}
+
+function isBetween(value, min, max) {
+    return (value >= min && value <= max);
 }
 
 
