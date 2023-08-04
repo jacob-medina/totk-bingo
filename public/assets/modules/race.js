778350af64b384568b13e5d3e897ae29803d6e7c
Jacob Medina <jacobmedina1618@gmail.com>
2023-08-04 16:10:43 -0400

    2 clients can join room, board syncs between clients

--- public/assets/modules/race.js
+++ public/assets/modules/race.js
@@ -1,9 +1,16 @@
+import { fetchAndGenerateBoard } from "./board.js";
+import { SeededRandom } from "./SeededRandom.js";
+import { updateShareURL } from "./share.js";
+
 let clientNum = 1;
 let room = null;
 
-function createRoom(socket) {
+function createRoom(socket, urlSearchParams) {
     const roomName = $('#create-room-name').val();
-    socket.emit('create-room', roomName);
+    socket.emit('create-room', {
+        room: roomName,
+        params: urlSearchParams.toString()
+    });
 
     socket.on('create-room-response', ({res, error}) => {
         if (error) {
@@ -17,7 +24,7 @@ function createRoom(socket) {
     });
 }
 
-function joinRoom(socket) {
+function joinRoom(socket, urlSearchParams) {
     const roomName = $('#join-room-name').val();
     socket.emit('join-room', roomName);
 
@@ -29,7 +36,15 @@ function joinRoom(socket) {
 
         room = roomName;
         clientNum = 2;
-        console.log(res);
+        console.log(res.message);
+        
+        urlSearchParams = new URLSearchParams(res.params);
+        const seed = urlSearchParams.get("seed");
+        const rand = new SeededRandom(seed);
+        $('#seed-input').val(seed);
+        updateShareURL();
+        console.log(seed);
+        fetchAndGenerateBoard(urlSearchParams, rand);
     });
 }
 
--- server.js
+++ server.js
@@ -15,29 +15,32 @@ io.on('connection', socket => {
     console.log(socket.id);
 
     // Create room
-    socket.on('create-room', (roomName) => {
-        if (rooms.find(room => room.name === roomName)) {
+    socket.on('create-room', ({room, params}) => {
+        if (rooms.find(rm => rm.name === room)) {
             socket.emit('create-room-response', {
-                error: `Room name ${roomName} already in use!`
+                error: `Room name ${room} already in use!`
             });
             return;
         }
 
+        console.log(params);
+
         rooms.push({
-            name: roomName,
+            name: room,
+            params: params,
             clients: 1
         });
 
-        socket.join(roomName);
+        socket.join(room);
 
         socket.emit('create-room-response', {
-            res: `Created room ${roomName}`
+            res: `Created room ${room}`
         });
     });
 
     // Join room
     socket.on('join-room', (roomName) => {
-        const room = rooms.find(room => room.name === roomName);
+        const room = rooms.find(rm => rm.name === roomName);
 
         if (room == null) {
             socket.emit('join-room-response', {
@@ -58,7 +61,10 @@ io.on('connection', socket => {
         socket.join(roomName);
 
         socket.emit('join-room-response', {
-            res: `Joined room ${roomName}`
+            res: {
+                message: `Joined room ${roomName}`,
+                params: room.params
+            }
         });
 
         console.info(rooms);
